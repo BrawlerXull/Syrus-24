32a34827ebd884c855666d3c323428d7edccc2ee
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-02-20 10:05:21 +0530

    navbar

--- clientt/src/App.js
+++ clientt/src/App.js
@@ -4,19 +4,21 @@ import {
     Routes,
     Route
 } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
+import Nav from "./components/Nav.jsx";
 
 
 
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home /> }/>
+        <Nav />
+        // <Router>
+        //     <Routes>
+        //         <Route path='/' element={<Home /> }/>
 
-            </Routes>
-        </Router>
+        //     </Routes>
+        // </Router>
     );
 }
 
--- clientt/src/pages/Home.jsx
+++ clientt/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Nav from '../components/Nav'
 
 function Home() {
   return (
-    <div className=' bg-black'>Home</div>
+    <>
+    <Nav />
+    
+    </>
   )
 }
 
